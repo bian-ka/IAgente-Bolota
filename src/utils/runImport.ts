caba662c2c4ee01cc0adf81f4eac375d79430569
Bianca Gomes <gomes18bianca@gmail.com>
2025-11-27 10:24:37 -0300

    fix: correct the use of environment variables for sensitive data

--- src/utils/runImport.ts
+++ src/utils/runImport.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import { importCSV } from './importCSV.js';
+import 'dotenv/config';
 
 const filePath = 'src/data/dados_produtos.csv';
 
 async function main() {
-	await mongoose.connect(
-		'mongodb://admin:bolota123@localhost:27017/bolota?authSource=admin'
-	);
+	const uri = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`;
+	await mongoose.connect(uri);
 	console.log('Connected to MongoDB');
 	await importCSV(filePath);
 	await mongoose.disconnect();
